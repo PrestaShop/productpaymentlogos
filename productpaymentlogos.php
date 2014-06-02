52a35c453ee1a646de396241236c30da56d8aa6e
Jerome Nadaud <jerome.nadaud@gmail.com>
2014-06-02 09:33:08 +0200

    //Version update

--- productpaymentlogos.php
+++ productpaymentlogos.php
@@ -33,7 +33,7 @@ class ProductPaymentLogos extends Module
 	{
 		$this->name = 'productpaymentlogos';
 		$this->tab = 'front_office_features';
-		$this->version = '1.3.2';
+		$this->version = '1.3.4';
 		$this->author = 'PrestaShop';
 		$this->need_instance = 0;
 
